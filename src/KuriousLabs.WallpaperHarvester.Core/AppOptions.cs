c119abf77de550fd4f29c53e31336d154695e751
Kia Raad <kia.panahirad@gmail.com>
2025-10-24 22:03:43 +0330

    Add MaxParallelism configuration option to AppOptions

--- src/KuriousLabs.WallpaperHarvester.Core/AppOptions.cs
+++ src/KuriousLabs.WallpaperHarvester.Core/AppOptions.cs
@@ -3,4 +3,5 @@ namespace KuriousLabs.WallpaperHarvester.Core;
 internal sealed record AppOptions
 {
     public string WallpaperDirectory { get; init; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Pictures", "Wallpapers");
+    public int MaxParallelism { get; init; } = 4;
 }
